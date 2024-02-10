dc0a0f41d0e25905fdb4144fa8be7b3b0c2f123b
Hanz <hanz@godot.id>
2024-02-10 13:19:20 +0700

    feat(ManagedCamera2D): Added docstring

--- addons/CameraManager2D/ManagedCamera2D.gd
+++ addons/CameraManager2D/ManagedCamera2D.gd
@@ -4,6 +4,8 @@ extends Node2D
 signal enabled_changed(cam: Node, prev: bool, now: bool)
 
 var cam = Camera2D.new()
+## Controls wheter the camera can be active or not.
+## Once ticked, other cameras in CameraManager2D would be disabled.
 @export
 var enabled: bool = false:
 	set(val):
