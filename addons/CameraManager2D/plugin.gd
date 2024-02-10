338054f4d2ee6c0ed7a34f31f436e1bd5fac342e
Hanz <hanz@godot.id>
2024-02-10 13:06:36 +0700

    feat: Added icons

--- addons/CameraManager2D/plugin.gd
+++ addons/CameraManager2D/plugin.gd
@@ -3,8 +3,8 @@ extends EditorPlugin
 
 
 func _enter_tree():
-	add_custom_type("CameraManager2D", "Node2D", preload("res://addons/CameraManager2D/CameraManager2D.gd"), preload("res://icon.svg"))
-	add_custom_type("ManagedCamera2D", "Node2D", preload("res://addons/CameraManager2D/ManagedCamera2D.gd"), preload("res://icon.svg"))
+	add_custom_type("CameraManager2D", "Node2D", preload("res://addons/CameraManager2D/CameraManager2D.gd"), preload("res://addons/CameraManager2D/icons/CameraManager2D.png"))
+	add_custom_type("ManagedCamera2D", "Node2D", preload("res://addons/CameraManager2D/ManagedCamera2D.gd"), preload("res://addons/CameraManager2D/icons/ManagedCamera2D.png"))
 
 
 func _exit_tree():
